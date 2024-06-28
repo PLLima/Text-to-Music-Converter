ed0856b71163d1e55c3b3c2acec1efa59c75092b
Pedro Lubaszewski Lima <74931964+PLLima@users.noreply.github.com>
2024-06-28 17:28:05 -0300

    feat: add screens module with start_screen package

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+'''
+Created on 2024-06-28
 
+@author: Pedro Lubaszewski Lima
+
+This is the starting point of the application, to where every major module is imported.
+'''
+
+import screens.start as start_screen
+
+if __name__ == '__main__':
+    start_screen
---
+++ src/screens/start/start_screen.py
@@ -0,0 +1,18 @@
+'''
+Created on 2024-06-28
+
+@author: Pedro Lubaszewski Lima
+
+This is the first screen that opens at the start of the application.
+'''
+
+import tkinter as tk
+from tkinter import tkk
+
+def start_screen():
+    None
+
+def test():
+    start_screen()
+if __name__ == '__main__':
+    test()
