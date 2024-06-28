f6053b117d6b87d2eb87ddbaa807a693c473648b
Pedro Lubaszewski Lima <74931964+PLLima@users.noreply.github.com>
2024-06-28 19:07:34 -0300

    feat: import basic screen with a button and text

--- src/screens/start/start_screen.py
+++ src/screens/start/start_screen.py
@@ -9,13 +9,21 @@ This is the first screen that opens at the start of the application.
 import tkinter as tk
 from tkinter import ttk
 
-def render():
-    root = tk.Tk()
-    tk.Label(root, text = 'Text to Music Converter').pack
+class start_screen():
 
-    root.mainloop()
+    def render():
+        root = tk.Tk()
+        root.wm_title("Text to Music Converter")
+        # tk.Label(root, text = 'Text to Music Converter').pack()
+
+        frm = ttk.Frame(root, padding = 10)
+        frm.grid()
+        ttk.Label(frm, text="Hello Worldsfaeafewfef!").grid(column = 0, row = 0)
+        ttk.Button(frm, text="Quit", command = root.destroy).grid(column = 1, row = 0)
+
+        root.mainloop()
 
 def test():
-    render()
+    start_screen.render()
 if __name__ == '__main__':
     test()
