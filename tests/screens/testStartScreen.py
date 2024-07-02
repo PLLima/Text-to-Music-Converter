ae00bd7b680047de3a63b2dedb3f2237b96d20f3
PLLima <74931964+PLLima@users.noreply.github.com>
2024-07-02 19:32:02 -0300

    tests: create test file for the startScreen class

---
+++ tests/screens/testStartScreen.py
@@ -0,0 +1,17 @@
+'''
+Created on 2024-07-02
+
+@author: Pedro Lubaszewski Lima
+
+File with automated unit tests for the startScreen class.
+'''
+
+import unittest
+
+import tkinter as tk
+from src.screens.start import startScreen
+
+
+
+if __name__ == '__main__':
+    unittest.main()
