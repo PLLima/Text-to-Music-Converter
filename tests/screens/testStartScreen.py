ce7d5d7a26ee8bd6d42cfff3afb45d9365841f3a
PLLima <74931964+PLLima@users.noreply.github.com>
2024-07-02 22:23:04 -0300

    test: add successfully render() test to testStartScreen()

--- tests/screens/testStartScreen.py
+++ tests/screens/testStartScreen.py
@@ -11,7 +11,16 @@ import unittest
 import tkinter as tk
 from src.screens.start import startScreen
 
+class testStartScreen(unittest.TestCase):
+    def setUp(self):
+        root = tk.Tk()
+        self.root = root
+        self.app = startScreen(root)
 
+    def testRender(self):
+        self.app.render()
+        self.app.update()
+        self.assertTrue(self.app.winfo_exists(), "Start screen wasn't rendered.")
 
 if __name__ == '__main__':
     unittest.main()
