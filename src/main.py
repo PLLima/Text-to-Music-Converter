4ce626f7edb42a0d8b53b2b258f5625d900e7684
PLLima <74931964+PLLima@users.noreply.github.com>
2024-07-09 19:37:35 -0300

    feat: limit app screen's size with a new method

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ Created on 2024-06-28
 This is the starting point of the application, to where every major module is imported.
 '''
 
+import re
 import tkinter as tk
 from screens.start import startScreen
 
@@ -20,11 +21,20 @@ class mainApplication(tk.Frame):
 
     def getAppName(self):
         return self.appName
-    
+
+    def setMinAppScreenSize(self, minSize):
+        splitMinSize = re.split('x', minSize)
+
+        self.appMinScreenSize = minSize
+        self.parent.minsize(splitMinSize[0], splitMinSize[1])
+
+    def getMinAppScreenSize(self):
+        return self.appMinScreenSize
+
     def setAppScreenSize(self, size):
         self.appScreenSize = size
         self.parent.geometry(size)
-    
+
     def getAppScreenSize(self):
         return self.appScreenSize
 
@@ -33,12 +43,15 @@ class mainApplication(tk.Frame):
         startScreen(self.parent).render()
 
 def main():
+    minScreenSize = ['400', '200']
+    initialScreenSize = ['400', '200']
     root = tk.Tk()
 
     # Set main screen parameters
     app = mainApplication(root)
     app.setAppName("Text to Music Converter")
-    app.setAppScreenSize("400x200")
+    app.setAppScreenSize("x".join(initialScreenSize))
+    app.setMinAppScreenSize("x".join(minScreenSize))
     
     app.begin()
     root.mainloop()
