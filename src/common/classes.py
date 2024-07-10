6fc9d84eb7e639df08d18a82970317dbb490a03f
PLLima <74931964+PLLima@users.noreply.github.com>
2024-07-10 00:45:58 -0300

    chore: add common classes file

---
+++ src/common/classes.py
@@ -0,0 +1,14 @@
+'''
+Created on 2024-07-10
+
+@author: Pedro Lubaszewski Lima
+
+Module with all the common classes of the project.
+'''
+
+class Child():
+    def setParent(self, parent):
+        self.parent = parent
+
+    def getParent(self):
+        return self.parent
