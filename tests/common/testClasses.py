0c14406515c3c32e4d12136ddcf8863482dc305d
PLLima <74931964+PLLima@users.noreply.github.com>
2024-07-10 00:46:15 -0300

    test: add tests to common classes

---
+++ tests/common/testClasses.py
@@ -0,0 +1,27 @@
+'''
+Created on 2024-07-10
+
+@author: Pedro Lubaszewski Lima
+
+File with automated unit tests for the class inside the classes.py file.
+'''
+
+import unittest
+
+from src.common.classes import Child
+
+class testChild(unittest.TestCase):
+    def setUp(self):
+        self.parent = 'Test'
+        self.child = Child()
+
+    def testSetParent(self):
+        self.child.setParent(self.parent)
+        self.assertEqual(self.child.parent, self.parent, "Parent incorrectly setted.")
+
+    def testGetParent(self):
+        self.child.setParent(self.parent)
+        self.assertEqual(self.child.getParent(), self.parent, "Gotten incorrect parent.")
+
+if __name__ == '__main__':
+    unittest.main()
