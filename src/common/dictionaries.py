b3ee5dbe7f6b05e41626dbd850f8b299191ae579
PLLima <74931964+PLLima@users.noreply.github.com>
2024-07-11 20:29:49 -0300

    refactor: change enums to dictionaries

---
+++ src/common/dictionaries.py
@@ -0,0 +1,20 @@
+'''
+Created on 2024-07-09
+
+@author: Pedro Lubaszewski Lima
+
+Module with all the common dictionaries of the project.
+'''
+
+SCREEN_MEASURE = {
+    "Width": 0,
+    "Height": 1
+    }
+
+TEXT_SCALES = {
+    "MainHeader": 0.0625
+}
+
+FONTS = {
+    "MainHeader": 'Inter'
+}
