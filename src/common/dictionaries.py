9253fbdc134d500ad20f8c47248e20e74838d765
PLLima <74931964+PLLima@users.noreply.github.com>
2024-08-06 17:46:41 -0300

    chore: add BUTTON_COLORS dictionary

--- src/common/dictionaries.py
+++ src/common/dictionaries.py
@@ -8,17 +8,31 @@ Module with all the common dictionaries of the project.
 
 SCREEN_MEASURE = {
     "Width": 0,
-    "Height": 1
+    "Height": 1,
     }
 
 TEXT_SCALES = {
     "MainHeader": 0.0625,
     "MainSubtitle": 0.026041667,
-    "TextButton": 0.026041667
+    "TextButton": 0.026041667,
 }
 
 FONTS = {
     "MainHeader": 'Courier New',
     "MainSubtitle": 'Courier New',
-    "TextButton": 'Courier New'
+    "TextButton": 'Courier New',
+}
+
+BUTTON_COLORS = {
+    "Black": '#000000',
+    "Gray": '#',
+    "Red": '#',
+
+    "Foreground": '#ffffff',
+    "BlackActive": '#2F2F2F',
+    "RedActive": '#FB8585',
+}
+
+COLORS = {
+    "TextButtonForeground": '#ffffff',
 }
