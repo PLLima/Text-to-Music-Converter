1234e0d197b06520531bf8c2a1e19767a504a7b7
PLLima <74931964+PLLima@users.noreply.github.com>
2024-08-08 15:17:26 -0300

    fix: make Closeable() interface work

--- src/common/classes.py
+++ src/common/classes.py
@@ -13,10 +13,11 @@ class Child():
     def getParent(self):
         return self.__parent
 
-class Destroyable():
-    def destroy(self):
+class Closeable():
+    def close(self):
         for widget in self.winfo_children():
             widget.destroy()
+        self.destroy()
 
 class WindowSizeNotifier():
     def __init__(self, window, eventHandler):
