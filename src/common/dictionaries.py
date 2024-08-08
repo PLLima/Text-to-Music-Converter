969fb44c2d07f37536814a2f25af247570af8840
PLLima <74931964+PLLima@users.noreply.github.com>
2024-08-08 10:27:15 -0300

    chore: change dictionaries scales

--- src/common/dictionaries.py
+++ src/common/dictionaries.py
@@ -12,11 +12,11 @@ SCREEN_MEASURE = {
     }
 
 TEXT_SCALES = {
-    "MainHeader": 0.0625,
-    "MainSubtitle": 0.026041667,
-    "TextButton": 0.026041667,
-    "ScreenTitle": 0.0525,
-    "TableContent": 0.025,
+    "MainHeader": 0.0725,
+    "MainSubtitle": 0.029041667,
+    "TextButton": 0.030041667,
+    "ScreenTitle": 0.0425,
+    "TableContent": 0.015,
 }
 
 TEXT_SCALES["TableHeader"] = TEXT_SCALES["TableContent"] + 0.005
