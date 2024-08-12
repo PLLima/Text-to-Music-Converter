e7dc6be5eea36ae08625635e234c4c0ce4bcb9d5
PLLima <74931964+PLLima@users.noreply.github.com>
2024-08-12 00:46:26 -0300

    refactor: create separate file for table widget

---
+++ src/common/widgets/table.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+
+from common.functions import calculateFontSize
+from common.dictionaries import TEXT_SCALES
+from common.dictionaries import FONTS
+
+class table():
+    def __init__(self, parent, screenSize):
+        # Table data
+        lst = [('Text', 'Musical Action or Instrument', 'Text', 'Musical Action or Instrument'),
+               ('A or a', 'Note A', '-', 'Reset volume to initial set value'),
+               ('B or b', 'Note B', 'O, o, U, u, I or i', 'If last character was a note then repeat it, else play MIDI instrument "Telephone Ring"(125)'),
+               ('C or c', 'Note C', 'R+', 'Increase one octave'),
+               ('D or d', 'Note D', 'R-', 'Decrease one octave'),
+               ('E or e', 'Note E', '?', 'Plays a random note'),
+               ('F or f', 'Note F', 'NL (New Line)', 'Change MIDI instrument randomly (between 1 and 128)'),
+               ('G or g', 'Note G', 'BPM+', 'Increase BPM by 80'),
+               ('Space', 'Silence or pause', ';', 'Change BPM to random value'),
+               ('+', 'Double the volume', 'Any other Character', 'Keep previous state')]
+        
+        total_rows = len(lst)
+        total_columns = len(lst[0])
+
+        border_thickness = 0.5
+        min_cell_width = 150
+        # Internal Label paddings
+        iPadding = 7
+
+        # Create table using bordered Labels
+        for i in range(total_rows):
+            for j in range(total_columns):
+                # Define an apropriate value for cell width
+                wraplength = min_cell_width
+                if i == 0:
+                    self.e = tk.Label(parent, fg='#636363', font=(FONTS["Table"], calculateFontSize(TEXT_SCALES["TableContent"], screenSize), 'bold'), 
+                                   text=lst[i][j], borderwidth=border_thickness, relief='solid', anchor='center',
+                                   wraplength=wraplength, justify='center', background='#dcdcdc')
+                else:
+                    self.e = tk.Label(parent, fg='#636363', font=(FONTS["Table"], calculateFontSize(TEXT_SCALES["TableContent"], screenSize), 'normal'), 
+                                   text=lst[i][j], borderwidth=border_thickness, relief='solid', anchor='center',
+                                   wraplength=wraplength, justify='center', background='#ffffff')
+                self.e.grid(row=i, column=j, padx=0, pady=0, sticky='nsew', ipadx=iPadding, ipady=iPadding)
+
+        # Adjust columns to have the same minimum width
+        for j in range(total_columns):
+            parent.grid_columnconfigure(j, weight=1, minsize=min_cell_width)
+        # Adjust lines to have the same height
+        for i in range(total_rows):
+            parent.grid_rowconfigure(i, weight=1)
--- src/screens/learnMore.py
+++ src/screens/learnMore.py
@@ -2,54 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 
 from common.classes import Screen
-from common.functions import calculateFontSize
-from common.dictionaries import TEXT_SCALES
-from common.dictionaries import FONTS
 from common.widgets.screenHeader import screenHeader
-
-class Table():
-    def __init__(self, parent, screenSize):
-        # Table data
-        lst = [('Text', 'Musical Action or Instrument', 'Text', 'Musical Action or Instrument'),
-               ('A or a', 'Note A', '-', 'Reset volume to initial set value'),
-               ('B or b', 'Note B', 'O, o, U, u, I or i', 'If last character was a note then repeat it, else play MIDI instrument "Telephone Ring"(125)'),
-               ('C or c', 'Note C', 'R+', 'Increase one octave'),
-               ('D or d', 'Note D', 'R-', 'Decrease one octave'),
-               ('E or e', 'Note E', '?', 'Plays a random note'),
-               ('F or f', 'Note F', 'NL (New Line)', 'Change MIDI instrument randomly (between 1 and 128)'),
-               ('G or g', 'Note G', 'BPM+', 'Increase BPM by 80'),
-               ('Space', 'Silence or pause', ';', 'Change BPM to random value'),
-               ('+', 'Double the volume', 'Any other Character', 'Keep previous state')]
-        
-        total_rows = len(lst)
-        total_columns = len(lst[0])
-
-        border_thickness = 0.5
-        min_cell_width = 150
-        # Internal Label paddings
-        iPadding = 7
-
-        # Create table using bordered Labels
-        for i in range(total_rows):
-            for j in range(total_columns):
-                # Define an apropriate value for cell width
-                wraplength = min_cell_width
-                if i == 0:
-                    self.e = tk.Label(parent, fg='#636363', font=(FONTS["Table"], calculateFontSize(TEXT_SCALES["TableContent"], screenSize), 'bold'), 
-                                   text=lst[i][j], borderwidth=border_thickness, relief='solid', anchor='center',
-                                   wraplength=wraplength, justify='center', background='#dcdcdc')
-                else:
-                    self.e = tk.Label(parent, fg='#636363', font=(FONTS["Table"], calculateFontSize(TEXT_SCALES["TableContent"], screenSize), 'normal'), 
-                                   text=lst[i][j], borderwidth=border_thickness, relief='solid', anchor='center',
-                                   wraplength=wraplength, justify='center', background='#ffffff')
-                self.e.grid(row=i, column=j, padx=0, pady=0, sticky='nsew', ipadx=iPadding, ipady=iPadding)
-
-        # Adjust columns to have the same minimum width
-        for j in range(total_columns):
-            parent.grid_columnconfigure(j, weight=1, minsize=min_cell_width)
-        # Adjust lines to have the same height
-        for i in range(total_rows):
-            parent.grid_rowconfigure(i, weight=1)
+from common.widgets.table import table
 
 class learnScreen(ttk.Frame, Screen):
     def __init__(self, appController):
@@ -67,7 +21,7 @@ class learnScreen(ttk.Frame, Screen):
         self.grid_columnconfigure(0, weight=1)
 
         # Add table widget to tableFrame
-        Table(tableFrame, self.getAppController().getScreenSize())
+        table(tableFrame, self.getAppController().getScreenSize())
 
         header.grid(row=0, column=0, sticky="NSEW", pady=20)
         titleFrame.grid(row=0, column=1, sticky="")
